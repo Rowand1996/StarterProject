18124b3e53effb50f9c438b26ab859946a0c82d2
Rowan Douglas <rowanandvera@gmail.com>
2020-09-18 12:25:41 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,8 +27,17 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<Persistence.DataContext>(opt => {
+            services.AddDbContext<Persistence.DataContext>(opt => 
+            {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
             });
             services.AddControllers();
         }
@@ -43,6 +52,7 @@ namespace API
 
             // app.UseHttpsRedirection();
 
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
