859c855f9b8337151e9ab4242d4905286a04961d
Rowan Douglas <rowanandvera@gmail.com>
2020-09-24 14:08:51 -0500

    end of section 6

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, Card, Image } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
+import { Button, Card, Image } from "semantic-ui-react";
 
 interface IProps {
   activity: IActivity;
@@ -14,31 +14,36 @@ export const ActivityDetails: React.FC<IProps> = ({
   setSelectedActivity,
 }) => {
   return (
-    <Card fluid>
-      <Image
-        src={`/assets/categoryImages/${activity.category}.jpg`}
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>{activity.title}</Card.Header>
-        <Card.Meta>
-          <span>{activity.date}</span>
-        </Card.Meta>
-        <Card.Description>{activity.description}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button.Group widths={2}>
-          <Button
-            onClick={() => setEditMode(true)}
-            basic
-            color="blue"
-            content="Edit"
-          />
-          <Button onClick={() => setSelectedActivity(null)} basic color="grey" content="Cancel" />
-        </Button.Group>
-      </Card.Content>
-    </Card>
+      <Card fluid id="StickyCard">
+        <Image
+          src={`/assets/categoryImages/${activity.category}.jpg`}
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>{activity.title}</Card.Header>
+          <Card.Meta>
+            <span>{activity.date}</span>
+          </Card.Meta>
+          <Card.Description>{activity.description}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Button.Group widths={2}>
+            <Button
+              onClick={() => setEditMode(true)}
+              basic
+              color="blue"
+              content="Edit"
+            />
+            <Button
+              onClick={() => setSelectedActivity(null)}
+              basic
+              color="grey"
+              content="Cancel"
+            />
+          </Button.Group>
+        </Card.Content>
+      </Card>
   );
 };
 
